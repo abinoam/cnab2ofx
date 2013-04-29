ed6efaf7894390dcdb4884800ecc7911c0d7a8ad
Abinoam Jr <abinoam@gmail.com>
2013-04-29 20:01:15 -0300

    Added Bankid and Branchid support (Banco e Agência)

--- cnab2ofx.rb
+++ cnab2ofx.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+#encoding:utf-8
 
 require 'yaml'
 require 'pp'
@@ -7,6 +8,12 @@ require 'erb'
 
 CONFIG_DIR = 'config'
 VIEW_DIR = 'view' 
+
+class String
+  def trim_lzeroes
+    self.to_i.to_s
+  end
+end
 
 class CNAB240
 
@@ -17,61 +24,73 @@ class CNAB240
     @cnab240 = parse
     @dtserver = get_dtserver
     @org = @fid = get_org
+    @bankid = get_bankid
+    @branchid = get_branchid
     @transactions = [{},{}]
+
   end
   
   def to_ofx
     ERB.new(File.read(File.join(VIEW_DIR, "extrato.ofx.erb"))).run(binding)
   end
-			
+  
   private
   
   def str_decode_with_headers(str, headers)
-		raise ArgumentError, "str.size should be 240 for CNAB240 but it's #{str.size}" if str.size != 240
-		str = str.dup
-		headers.each_with_object({}) do |(k,v), hsh| 
-			hsh[k.to_sym] = str.slice!(0,v) 
-		end
-	end
-	
-	def parse
-    
+    raise ArgumentError, "str.size should be 240 for CNAB240 but it's #{str.size}" if str.size != 240
+    str = str.dup
+    headers.each_with_object({}) do |(k,v), hsh| 
+      hsh[k.to_sym] = str.slice!(0,v) 
+    end
+  end
+  
+  def parse
+
     f = File.open(@filename)
     
     lines = {}
 
-		lines[:header_de_arquivo],
-		lines[:header_de_lote],
-		*lines[:detalhe_segmento_e],
-		lines[:trailer_de_lote],
-		lines[:trailer_de_arquivo] = f.readlines
-		
-		lines.each_with_index.with_object({}) do |((k,v), i), hsh| #|(k,v), i, hsh|
-			file_index = "%03d" % i
-			fields_headers = YAML.load_file (File.join(CONFIG_DIR, "#{file_index}_registro_#{k}.yaml"))
-			case v
-			when String
-				hsh[k] = str_decode_with_headers(v.chomp, fields_headers)
-			when Array
-				hsh[k] = v.map {|line| str_decode_with_headers(line.chomp, fields_headers) }
-			else
-				raise ArgumentError, "v (line) should be Array or String but it is #{v.class} "
-			end
-		end
-	end
-	
-	def get_dtserver
-		cnab_date_string = @cnab240[:header_de_arquivo][:data_arquivo]+@cnab240[:header_de_arquivo][:hora_arquivo]
-		cnab_dtserver = DateTime.strptime(cnab_date_string,"%d%m%Y%H%M%S") # Import CNAB DateTime format
-		@dtserver = cnab_dtserver.strftime("%Y%m%d%H%M%S") # Export OFX DateTime format
-	end
-	
-	def get_org
-	  @org = @fid = @cnab240[:header_de_arquivo][:nome_banco].strip
-	end
-	
-	alias get_fid get_org
-	
+    lines[:header_de_arquivo],
+    lines[:header_de_lote],
+    *lines[:detalhe_segmento_e],
+    lines[:trailer_de_lote],
+    lines[:trailer_de_arquivo] = f.readlines
+    
+    lines.each_with_index.with_object({}) do |((k,v), i), hsh| #|(k,v), i, hsh|
+      file_index = "%03d" % i
+      fields_headers = YAML.load_file (File.join(CONFIG_DIR, "#{file_index}_registro_#{k}.yaml"))
+      case v
+      when String
+        hsh[k] = str_decode_with_headers(v.chomp, fields_headers)
+      when Array
+        hsh[k] = v.map {|line| str_decode_with_headers(line.chomp, fields_headers) }
+      else
+        raise ArgumentError, "v (line) should be Array or String but it is #{v.class} "
+      end
+    end
+  end
+
+
+  def get_dtserver
+    cnab_date_string = @cnab240[:header_de_arquivo][:data_arquivo]+@cnab240[:header_de_arquivo][:hora_arquivo]
+    cnab_dtserver = DateTime.strptime(cnab_date_string,"%d%m%Y%H%M%S") # Import CNAB DateTime format
+    @dtserver = cnab_dtserver.strftime("%Y%m%d%H%M%S") # Export OFX DateTime format
+  end
+
+  def get_org
+    @org = @fid = @cnab240[:header_de_arquivo][:nome_banco].strip
+  end
+
+  alias get_fid get_org
+
+  def get_bankid
+    @cnab240[:header_de_arquivo][:banco].trim_lzeroes
+  end
+
+  def get_branchid
+    @cnab240[:header_de_arquivo][:agência].trim_lzeroes + "-" +
+    @cnab240[:header_de_arquivo][:agência_dv].trim_lzeroes
+  end
 end
 
 
@@ -85,34 +104,3 @@ pp cnab240.dtserver
 pp cnab240.fid
 pp cnab240.org
 pp cnab240.to_ofx
-
-
-
-exit
-
-
-pp decoded
-
-
-
-
-
-@transactions = Array.new(5) { Hash.new }
-
-e = ERB.new(File.read("extrato.ofx.erb")).run
-
-puts e
-
-pp date_string
-pp cnab_dtserver
-
-pp @dtserver
-
-
-
-=begin
-@dtserver 20130420152834[-3:GMT]
-
-@org e @fid SANTANDER
-
-=end
