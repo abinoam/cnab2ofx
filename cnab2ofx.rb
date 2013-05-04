2bfa7909006a5fe8203fa87034d6ced9fb4178e1
Abinoam Jr <abinoam@gmail.com>
2013-05-04 14:15:25 -0300

    Moved String helper code to a separate file

--- cnab2ofx.rb
+++ cnab2ofx.rb
@@ -5,6 +5,7 @@ require 'yaml'
 require 'pp'
 require 'date'
 require 'erb'
+require './helpers/cnab240_helper.rb' 
 
 CONFIG_DIR = 'config'
 VIEW_DIR   = 'view' 
@@ -12,18 +13,7 @@ VIEW_DIR   = 'view'
 #TODO: Solve the encoding problem
 
 class String
-  def trim_lzeroes
-    self.sub(/^0+/,"")
-  rescue ArgumentError => e
-    i = self.encode("utf-8", "iso-8859-1") # Brazilian encoding used by some banks
-    i.sub(/^0+/,"")
-    i.encode("iso-8859-1", "utf-8")
-  end
-
-  def date_convert(from, to)
-    from_date = DateTime.strptime(self,from) # Import from 'from' DateTime format
-    to_date = from_date.strftime(to) # Export to 'to' DateTime format
-  end
+  include CNAB240Helper::StringHelper
 end
 
 class CNAB240
